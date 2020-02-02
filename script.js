95f5c3d57d49c2ec64310ab73027a56fcbd87f0d
Ilya <klyukovskiy@yandex.ru>
2020-02-02 23:36:22 +0300

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+// В переменную cards поместить класс карточек и вызвать функцию
+
+function autoHeight(cards) {
+	
+    let cardsContent = document.querySelectorAll(cards);
+    let arr = [];
+    for (let index = 0; index < cardsContent.length; index++) {
+        let style = getComputedStyle(cardsContent[index]);
+        
+        arr.push(style.height);
+    }
+    
+    let newArr = [];
+    arr.forEach(element => {
+        let t = element.replace('px','');
+        newArr.push(t);
+    });
+    
+    
+    let numArr = [];
+    newArr.forEach(element => {
+        numArr.push(+element);
+    });
+    
+    
+    let maxHeight = (Math.max.apply(null, numArr));
+     
+    cardsContent.forEach(element => {
+        element.style.height = `${maxHeight}px`;
+    });
+    }
+    
